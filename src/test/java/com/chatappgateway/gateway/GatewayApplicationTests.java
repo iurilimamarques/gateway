92dd3d4ab8230d87d7443ba565ce1858a7b652b6
Iuri de Lima Marques <iurilmarques@hotmail.com>
2022-03-13 16:25:48 -0300

    master - removing unit test

--- src/test/java/com/chatappgateway/gateway/GatewayApplicationTests.java
+++ src/test/java/com/chatappgateway/gateway/GatewayApplicationTests.java
@@ -1,13 +1,7 @@
 package com.chatappgateway.gateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
